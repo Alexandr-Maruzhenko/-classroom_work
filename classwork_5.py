02dd50be04e4968b66e29fc7031eff81a530145a
Alexandr <alexandr780113@gmail.com>
2024-02-05 21:44:32 +0300

    First commit

---
+++ classwork_5.py
@@ -0,0 +1,23 @@
+# x = 1
+# y = 0
+# z = 0
+# if not x or not y and z:
+#     print(1)
+# elif not x or not y and z:
+#     print (2)
+# elif not x or y or not y and x:
+#     print (3)
+# else:
+#     print (4)
+
+# while not s.isnumeric():
+#     s = input()
+#     print("Попробуйте ещё")
+
+s = input()
+obj = []
+
+for i in s:
+    obj.append(i.upper())
+
+print(obj)
