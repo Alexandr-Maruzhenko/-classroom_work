9da553c93d9b4bc3e52dfbb5fa901b829b20692e
Alexandr <alexandr780113@gmail.com>
2024-02-11 01:18:48 +0300

    First commit

--- classwork_6.py
+++ classwork_6.py
@@ -1,1 +1,11 @@
-print()
+def up(string):
+    if isinstance(string, str):
+        return string.upper()
+    else:
+        raise TypeError(f"Введена не строка")
+
+
+try:
+    print(up(1))
+except TypeError as exc:
+    print(f"Возникла ошибка: {exc}")
